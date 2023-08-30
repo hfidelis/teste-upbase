e1a60e44a0f4715a804bc8e4ac8549af6ecef6cb
Heitor Fidelis <heitorc88@gmail.com>
2023-08-30 01:34:45 -0300

    feat: added professional card component

---
+++ src/app/components/professional-card/professional-card.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfessionalCardComponent } from './professional-card.component';
+
+describe('ProfessionalCardComponent', () => {
+  let component: ProfessionalCardComponent;
+  let fixture: ComponentFixture<ProfessionalCardComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProfessionalCardComponent]
+    });
+    fixture = TestBed.createComponent(ProfessionalCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
