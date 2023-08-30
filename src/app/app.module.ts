28ac741431e2eaa27801a51bcff63c0832cbe2be
Heitor Fidelis <heitorc88@gmail.com>
2023-08-30 01:35:21 -0300

    adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BottomBarComponent } from './components/bottom-bar/bottom-bar.component';
 import { NavLabelComponent } from './components/nav-label/nav-label.component';
+import { ProfessionalCardComponent } from './components/professional-card/professional-card.component';
+import { ShiftsComponent } from './pages/shifts/shifts.component';
+import { StatusComponent } from './components/status/status.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BottomBarComponent,
-    NavLabelComponent
+    NavLabelComponent,
+    ProfessionalCardComponent,
+    ShiftsComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/shifts/shifts.component.ts
+++ src/app/pages/shifts/shifts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Professional } from 'src/app/models/Professional';
 
 @Component({
   selector: 'app-shifts',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shifts.component.scss']
 })
 export class ShiftsComponent {
-
+  dr_carolina: Professional = {
+    name: 'Carolina Magalhães',
+    picture: 'assets/profile-1.jpg'
+  }
 }
